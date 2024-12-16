7c60f73caa18a686d04d6fa1b49ab05f5bf9bc6f
John E Jones IV <john@johnjonesfour.com>
2024-12-16 16:29:17 -0500

    colors

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <credentials.h>
 #include <Clock.h>
 #include <Face.h>
-#include <GradientColor.h>
 
 #define OUTER_RING  14
 #define INNER_RING  12
@@ -12,11 +11,17 @@
 #define COLOR_ORDER GRB
 #define BRIGHTNESS  255
 
+#define SECONDS_IN_MINUTE 60.0
+#define SECONDS_IN_HOUR (60.0 * 60.0)
+#define SECONDS_IN_DAY (60.0 * 60.0 * 24.0)
+
 Clock clock1(UTC_OFFSET, USE_DST);
-float hues[3] = {0.65,0.12,0};
-GradientColor c(hues, 3, 1, 0.5);
 Face outer(60, 2);
 Face inner(24, 1);
+
+int secondStep = 0;
+int minuteStep = 0;
+int hourStep = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -41,18 +46,69 @@ void setup() {
   FastLED.setBrightness( BRIGHTNESS );
 }
 
+float hue_to_rgb(float p, float q, float t) {
+    if (t < 0.0f) t += 1.0f;
+    if (t > 1.0f) t -= 1.0f;
+    if (t < 1.0f / 6.0f) return p + (q - p) * 6.0f * t;
+    if (t < 1.0f / 2.0f) return q;
+    if (t < 2.0f / 3.0f) return p + (q - p) * (2.0f / 3.0f - t) * 6.0f;
+    return p;
+}
+
+// Function to convert HSL to RGB
+void hsl_to_rgb(float h, float s, float l, CRGB* rgb) {
+    float q, p;
+
+    if (s == 0.0f) {
+        // Achromatic (grey)
+        rgb->r = rgb->g = rgb->b = (uint8_t)(l * 255.0f);
+    } else {
+        if (l < 0.5f) {
+            q = l * (1.0f + s);
+        } else {
+            q = l + s - l * s;
+        }
+        p = 2.0f * l - q;
+
+        float red = hue_to_rgb(p, q, h + 1.0f / 3.0f);
+        float green = hue_to_rgb(p, q, h);
+        float blue = hue_to_rgb(p, q, h - 1.0f / 3.0f);
+
+        // Convert from float [0, 1] to uint8_t [0, 255]
+        rgb->r = (uint8_t)(red * 255.0f);
+        rgb->g = (uint8_t)(green * 255.0f);
+        rgb->b = (uint8_t)(blue * 255.0f);
+    }
+}
+
 void loop() {
   struct tm timeinfo;
   clock1.update(&timeinfo);
 
-  float pct = (float)(timeinfo.tm_sec + (timeinfo.tm_min * 60) + (timeinfo.tm_hour * 60 * 60)) / 86400.0;
+  float secondPct = (float)((timeinfo.tm_sec + secondStep) % 60) / SECONDS_IN_MINUTE;
+  float minutePct = (float)(timeinfo.tm_sec + (((timeinfo.tm_min + minuteStep) % 60) * 60)) / SECONDS_IN_HOUR;
+  float hourPct = (float)(timeinfo.tm_sec + (timeinfo.tm_min * 60) + (((timeinfo.tm_hour + hourStep) % 24) * 60 * 60)) / SECONDS_IN_DAY;
 
-  CRGB color;
-  c.getColor(pct, &color);
+  if (secondPct == 0) {
+    secondStep++;
+  }
+  if (minuteStep == 0) {
+    minuteStep++;
+  }
+  if (hourStep == 0) {
+    hourStep++;
+  }
 
-  outer.setHand(0, handpos{.index = timeinfo.tm_sec,            .color = color});
-  outer.setHand(1, handpos{.index = timeinfo.tm_min,            .color = color});
-  inner.setHand(0, handpos{.index = timeinfo.tm_hour % 12 * 2,  .color = color});
+  CRGB second;
+  CRGB minute;
+  CRGB hour;
+  hsl_to_rgb(secondPct, 1, 0.5, &second);
+  hsl_to_rgb(minutePct, 1, 0.5, &minute);
+  hsl_to_rgb(hourPct, 1, 0.5, &hour);
+
+  outer.setHand(0, handpos{.index = timeinfo.tm_sec,            .color = second});
+  outer.setHand(1, handpos{.index = timeinfo.tm_min,            .color = minute});
+  inner.setHand(0, handpos{.index = timeinfo.tm_hour % 12 * 2,  .color = hour});
 
   FastLED.show();
   FastLED.delay(10);
